803cc0a0211e0006d6597799a1266ac1f16547d4
ionvop <ionvop@gmail.com>
2026-08-25 22:47:29 +0800

    Update main.tsx

--- resources/js/main.tsx
+++ resources/js/main.tsx
@@ -1,6 +1,5 @@
 import "@vitejs/plugin-react/preamble";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "../css/globals.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
