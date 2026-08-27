d118edc370123f387c3fef36880a1ace3bc6fe7e
ionvop <ionvop@gmail.com>
2026-08-27 21:03:21 +0800

    Update 2026_07_19_160004_create_settings_table.php

--- database/migrations/2026_07_19_160004_create_settings_table.php
+++ database/migrations/2026_07_19_160004_create_settings_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('settings', function (Blueprint $table) {
             $table->string('key')->primary();
             $table->text('value')->nullable();
+            $table->timestamps();
         });
     }
 
