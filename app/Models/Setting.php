a8557be557081d9f00f714f1966f8084d3ec65f2
ionvop <ionvop@gmail.com>
2026-08-31 23:29:58 +0800

    Update Setting.php

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Setting extends Model
 {
+    use HasFactory;
+
     protected $primaryKey = 'key';
     public $incrementing = false;
     protected $keyType = 'string';
