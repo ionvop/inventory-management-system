10f9249d2c216d2c76328daf5327bf72774780ab
ionvop <ionvop@gmail.com>
2026-08-31 23:30:00 +0800

    Update Transaction.php

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Transaction extends Model
 {
+    use HasFactory;
+
     protected $fillable = ['item_id', 'user_id', 'movement', 'quantity', 'posted_at'];
     protected $casts = ['posted_at' => 'datetime'];
 
