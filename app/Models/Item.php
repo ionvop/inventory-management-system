e8957cb1a8159311696bd1cd6312c4e3cb25f337
ionvop <ionvop@gmail.com>
2026-08-31 23:29:56 +0800

    Update Item.php

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Item extends Model
 {
+    use HasFactory;
+
     protected $fillable = ['name', 'unit', 'minimum_stock'];
     protected $appends = ['current_stock', 'is_low_stock'];
 
