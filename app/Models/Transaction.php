8d03ffdb4bd43c228c04acb7c1634ffa8943db4b
ionvop <ionvop@gmail.com>
2026-09-01 10:08:34 +0800

    Update Transaction.php

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -11,6 +11,7 @@ class Transaction extends Model
 
     protected $fillable = ['item_id', 'user_id', 'movement', 'quantity', 'posted_at'];
     protected $casts = ['posted_at' => 'datetime'];
+    protected $appends = ['stock_after'];
 
     public function item()
     {
@@ -20,5 +21,15 @@ class Transaction extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Resulting stock for the item AFTER this transaction.
+     * Populated by TransactionController::attachStockAfter() for list
+     * responses; null when not computed (e.g. single-resource responses).
+     */
+    public function getStockAfterAttribute(): ?int
+    {
+        return $this->attributes['stock_after'] ?? null;
     }
 }
