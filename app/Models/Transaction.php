c72b100b62f13758da2f09cecf91fa086f7f5331
ionvop <ionvop@gmail.com>
2026-09-18 20:17:28 +0800

    Update Transaction.php

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -11,7 +11,7 @@ class Transaction extends Model
 
     protected $fillable = ['item_id', 'user_id', 'movement', 'quantity', 'posted_at'];
     protected $casts = ['posted_at' => 'datetime'];
-    protected $appends = ['stock_after'];
+    protected $appends = ['stock_after', 'bid_after'];
 
     public function item()
     {
@@ -31,5 +31,15 @@ class Transaction extends Model
     public function getStockAfterAttribute(): ?int
     {
         return $this->attributes['stock_after'] ?? null;
+    }
+
+    /**
+     * Resulting bid quantity for the item AFTER this transaction.
+     * Populated by TransactionController::attachStockAfter() for list
+     * responses; null when not computed (e.g. single-resource responses).
+     */
+    public function getBidAfterAttribute(): ?int
+    {
+        return $this->attributes['bid_after'] ?? null;
     }
 }
