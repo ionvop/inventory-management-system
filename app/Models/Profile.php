f7a1c44f0e01af1dccc151056e285d374cfc72c0
ionvop <ionvop@gmail.com>
2026-09-25 14:27:45 +0800

    Create Profile.php

---
+++ app/Models/Profile.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\ProfileFactory;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Carbon;
+
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $role
+ * @property Carbon|null $deleted_at
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ */
+#[Fillable(['name', 'role'])]
+#[Hidden(['deleted_at'])]
+class Profile extends Model
+{
+    /** @use HasFactory<ProfileFactory> */
+    use HasFactory, SoftDeletes;
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'deleted_at' => 'datetime',
+        ];
+    }
+
+    /**
+     * The transactions recorded by this profile.
+     *
+     * @return HasMany<Transaction, $this>
+     */
+    public function transactions(): HasMany
+    {
+        return $this->hasMany(Transaction::class);
+    }
+}
