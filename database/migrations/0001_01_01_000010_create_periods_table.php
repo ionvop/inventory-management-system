c8486f8a602bdb7c9fee7c6d19f220d09434a609
ionvop <ionvop@gmail.com>
2026-09-25 14:29:18 +0800

    Create 0001_01_01_000010_create_periods_table.php

---
+++ database/migrations/0001_01_01_000010_create_periods_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('periods', function (Blueprint $table) {
+            $table->id();
+            $table->integer('year');
+            $table->integer('month');
+            $table->enum('status', ['open', 'closed'])->default('open');
+            $table->timestamp('closed_at')->nullable();
+            $table->foreignId('closed_by')->nullable();
+            $table->string('reopened_reason')->nullable();
+            $table->timestamps();
+
+            $table->unique(['year', 'month']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('periods');
+    }
+};
