990e45bdce5dda74832b9cbc8bc30a266372e953
ionvop <ionvop@gmail.com>
2026-09-25 14:27:47 +0800

    Create Supplier.php

---
+++ app/Models/Supplier.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\SupplierFactory;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Carbon;
+
+/**
+ * @property int $id
+ * @property string $name
+ * @property string|null $contract_status
+ * @property bool $active
+ * @property Carbon|null $deleted_at
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ */
+#[Fillable(['name', 'contract_status', 'active'])]
+#[Hidden(['deleted_at'])]
+class Supplier extends Model
+{
+    /** @use HasFactory<SupplierFactory> */
+    use HasFactory;
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'active' => 'boolean',
+            'deleted_at' => 'datetime',
+        ];
+    }
+
+    /**
+     * The supplier items offered by this supplier.
+     *
+     * @return HasMany<SupplierItem, $this>
+     */
+    public function supplierItems(): HasMany
+    {
+        return $this->hasMany(SupplierItem::class);
+    }
+}
